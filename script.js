172775b889f215f12ef91803a15575554eee3a42
Not FolikoKiril <121515137+Belugono@users.noreply.github.com>
2025-05-17 01:05:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,12 @@ const video = document.getElementById('video');
 let conn = null;
 let call = null;
 
+// При получении собственного ID
 peer.on('open', id => {
   myIdEl.textContent = id;
 });
 
-peer.on('call', incomingCall => {
-  incomingCall.answer(); // ответить без потока (если только принимаем)
-  incomingCall.on('stream', stream => {
-    video.srcObject = stream;
-  });
-});
-
+// Подключение к другому пользователю
 connectBtn.onclick = () => {
   const remoteId = remoteIdInput.value;
   conn = peer.connect(remoteId);
@@ -28,6 +23,7 @@ connectBtn.onclick = () => {
   });
 };
 
+// Кнопка "Поделиться экраном"
 shareBtn.onclick = async () => {
   const stream = await navigator.mediaDevices.getDisplayMedia({ video: { frameRate: 30 } });
   video.srcObject = stream;
@@ -36,7 +32,22 @@ shareBtn.onclick = async () => {
   call = peer.call(remoteId, stream);
 };
 
+// Кнопка "На весь экран"
 fullscreenBtn.onclick = () => {
   if (video.requestFullscreen) video.requestFullscreen();
   else if (video.webkitRequestFullscreen) video.webkitRequestFullscreen();
 };
+
+// Обработка входящего вызова
+peer.on('call', incomingCall => {
+  const confirmResult = confirm(`Пользователь ${incomingCall.peer} хочет показать вам свой экран. Принять?`);
+  
+  if (confirmResult) {
+    incomingCall.answer(); // Принимаем без отправки своего потока
+    incomingCall.on('stream', stream => {
+      video.srcObject = stream;
+    });
+  } else {
+    console.log('Пользователь отказался принимать экран.');
+  }
+});
